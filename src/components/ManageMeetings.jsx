421d0965c59ac1716b008472f085a87d5d18c3cd
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-03-26 09:40:50 +0100

    initial components in place

---
+++ src/components/ManageMeetings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MeetingForm from './MeetingForm';
+import MeetingsTable from './MeetingsTable';
+
+const ManageMeetings = () => {
+    return (
+        <>
+        <MeetingForm />
+        <MeetingsTable />
+        </>
+    );
+};
+
+export default ManageMeetings;
---
+++ src/components/MeetingForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MeetingForm = () => {
+    return (
+        <div>
+            <span>Schedule new meeting</span>
+        </div>
+    );
+};
+
+export default MeetingForm;
