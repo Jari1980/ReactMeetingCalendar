8e63c9837b190cedd06a8720a85d0e38327e13c2
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-04-02 10:05:06 +0200

    Home page with navigation to dahboard, as of now meetingschedule

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import ManageMeetings from "./components/ManageMeetings";
 import "bootstrap/dist/css/bootstrap.css";
 import { MeetingProvider } from "./components/context";
 import NavBarComp from "./components/NavBarComp";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <NavBarComp />
     <MeetingProvider>
-      <ManageMeetings />
+    <Routes>
+    <Route path="/" elemet={<Home />} />
+    <Route path="/home" element={<Home />} />
+      <Route path="dashboard/*" element={<ManageMeetings />} />
+    </Routes>
     </MeetingProvider>
   </BrowserRouter>
 );
